b9b0a7e76b7179035424e49cfa8947905e5ccb1a
info@dgt-international.com <info@dgt-international.com>
2025-12-31 13:42:11 -0500

    captured intent with 1st shot of the end

--- 04_Code/feature_engineering.py
+++ 04_Code/feature_engineering.py
@@ -37,3 +37,32 @@ stones_data.loc[stones_data['defensive_intent'].isna(),
 # %%
 # creating a end-level table
 
+# columns should be is_power_play, defensing_team, defensive_posutre
+
+# random samples to understand the data
+print(stones_data.shape)
+n_rows = len(stones_data)
+random_index = np.random.randint(0, n_rows)
+stones_data.iloc[random_index]
+start_index = np.random.randint(0, n_rows - 30)
+stones_data.iloc[start_index:start_index + 30]
+
+# %%
+stones_data.head(30)
+
+# %%
+shots_by_team = ['CompetitionID', 'SessionID', 'GameID', 'EndID', 'TeamID']
+stones_data['defensive_intent_shot1'] = stones_data.groupby(shots_by_team)['defensive_intent'].transform('first')
+stones_data.head(30)
+
+# %%
+cols_to_show = list(stones_data.loc[:, 'CompetitionID':'Task'].columns) + ['defensive_intent', 'defensive_intent_shot1']
+stones_data[cols_to_show].head(30)
+
+# %%
+stones_data['defensive_intent'].value_counts(normalize=True) *100
+
+# %%
+stones_data['defensive_intent_shot1'].value_counts(normalize=True)* 100
+
+# %%
